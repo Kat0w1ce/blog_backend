fe49b765a358ef6f85e38dfb18257631da074293
Kat0w1ce <kato.szy@gmail.com>
2021-01-25 21:05:58 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use actix_web::{App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(index).service(blog))
-        .bind("localhost:9999")?
+        .bind("0.0.0.0:9999")?
         .run()
         .await
 }
